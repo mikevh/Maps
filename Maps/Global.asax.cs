b606729f96b6abacb1991bd8bba079e7c481163e
Michael <mikevh@gmail.com>
2013-10-13 14:54:59 -0700

    Self-referencing json serialization fix

--- Maps/Global.asax.cs
+++ Maps/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Newtonsoft.Json;
 
 namespace Maps
 {
@@ -21,6 +22,9 @@ namespace Maps
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+                ReferenceLoopHandling.Ignore;
         }
     }
 }
