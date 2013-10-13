185e7345a2f57df0e0cf3c5fbdf76bde061746d3
Michael <mikevh@gmail.com>
2013-10-12 20:49:01 -0700

    showing locations on map through webapi call

---
+++ Maps/Controllers/api/LocationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Maps.Models;
+
+namespace Maps.Controllers.api
+{
+    public class LocationController : ApiController
+    {
+        private MapsEntities _db = new MapsEntities();
+
+        // GET api/location
+        public IEnumerable<Location> Get()
+        {
+            var rv = _db.Locations;
+            return rv;
+        }
+
+        // GET api/location/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/location
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/location/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/location/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
