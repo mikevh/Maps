2a961969532623132a964758e3afd8fe1ffacb14
Michael <mikevh@gmail.com>
2013-10-13 17:41:00 -0700

    index angular work

--- Maps/Scripts/index.js
+++ Maps/Scripts/index.js
@@ -1,44 +1,46 @@
 ﻿var app = angular.module('maps', []);
-var map;
-var openInfoWindow;
 
-initialize();
+app.controller('Index', function($scope, $http) {
 
-app.controller('Index', function($scope) {
-    $.get('/api/location').success(function(data) {
-        showMarkers(data);
-    })
-});
+    var map;
+    var openInfoWindow;
 
-function initialize() {
-    var mapOptions = {
-        zoom: 7,
-        center: new google.maps.LatLng(47.22, -120.72),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    $scope.showMarkers = function(locations) {
+        locations.forEach(function(loc) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(loc.Latitude, loc.Longitude),
+                map: map,
+                title: loc.Name,
+                animation: google.maps.Animation.DROP
+            });
+
+            var contentString = '<a href="/Location/Details/' + loc.Id + '">' + loc.Name + '</a>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                if (openInfoWindow != null) {
+                    openInfoWindow.close();
+                }
+                openInfoWindow = infowindow;
+                infowindow.open(map, marker);
+            });
+        });
     };
-    map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-}
 
-function showMarkers(locations) {
-    locations.forEach(function (loc) {
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(loc.Latitude, loc.Longitude),
-            map: map,
-            title: loc.Name,
-            animation: google.maps.Animation.DROP
-        });
+    $scope.initialize = function() {
+        var mapOptions = {
+            zoom: 7,
+            center: new google.maps.LatLng(47.22, -120.72),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+    };
 
-        var contentString = '<a href="/Location/Details/' + loc.Id + '">' + loc.Name + '</a>';
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-        });
+    $scope.initialize();
 
-        google.maps.event.addListener(marker, 'click', function () {
-            if (openInfoWindow != null) {
-                openInfoWindow.close();
-            }
-            openInfoWindow = infowindow;
-            infowindow.open(map, marker);
-        });
-    });
-}
+    $http.get('/api/location').success(function (data) {
+        $scope.showMarkers(data);
+    }).error(function(data) { alert(data); });
+});
